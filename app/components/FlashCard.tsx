a1224102350b62ddf973d5c92593224aca0d0ddb
luzivaltic <gokuhdvn3@gmail.com>
2023-11-18 13:47:49 +0700

    rebase: merge luzi_nav_bar to develop

---
+++ app/components/FlashCard.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@mui/material";
+
+export const FlashCard = () => {
+    return (
+        <>
+            <h1>Lalala</h1>
+            <h2>Lalag</h2>
+            <Card sx={{
+                display: "flex",
+                background: "red",
+            }} > lalaland </Card>
+        </>
+        );
+};
